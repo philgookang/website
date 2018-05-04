4efa7454905904af4666dde20b004175adea2bf7
Phil Goo Kang <philgookang@gmail.com>
2018-05-04 21:24:03 +0900

    added listing article to home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,8 +3,14 @@
 class Home extends CI_Controller {
 
 	public function index() {
+
+        $article_list = ArticleM::new()->getList();
+
+        $data = array();
+        $data['article_list'] = $article_list;
+
 		$this->load->view('website/template/head');
-        $this->load->view('website/page/home');
+		$this->load->view('website/page/article_list', $data);
         $this->load->view('website/template/sidebar');
         $this->load->view('website/template/foot');
 	}
