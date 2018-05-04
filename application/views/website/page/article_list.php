00750b5645fffd7a4ada307461ee158e358e88d3
Phil Goo Kang <philgookang@gmail.com>
2018-05-04 21:24:29 +0900

    added search form to article page

--- application/views/website/page/article_list.php
+++ application/views/website/page/article_list.php
@@ -1,5 +1,36 @@
 <div class="row">
     <div class="col-md-8 blog-main">
+
+        <?php if (isset($search) && $search) { ?>
+            <form method="GET" action="/search/">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h3>Search</h3>
+                    </div>
+                </div>
+                <!--/.row-->
+                <div class="row">
+                    <div class="col-lg-9">
+                        <input type="text" class="form-control" name="search_text" value="<?php echo $this->input->get('search_text'); ?>" />
+                    </div>
+                    <!--/.col-lg-9-->
+                    <div class="col-lg-3">
+                        <input type="submit" class="btn btn-block btn-primary" value="Search" />
+                    </div>
+                    <!--/.col-lg-3-->
+                </div>
+                <!--/.row-->
+            </form>
+
+            <br /><hr></hr><br />
+        <?php } ?>
+
+        <?php if (count($article_list) <= 0) { ?>
+            <div class="blog-post">
+                <p>No Search Results</p>
+            </div>
+        <?php } ?>
+
         <?php foreach($article_list as $article) { ?>
             <div class="blog-post">
                 <h2 class="blog-post-title"><?php echo $article->title; ?></h2>
