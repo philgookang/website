ed48027d38ef0b13d8c8bd2114ae65beeecf914f
Phil Goo Kang <philgookang@gmail.com>
2018-05-30 15:01:39 +0900

    removed jumbotron from all pages

--- application/views/website/template/head.php
+++ application/views/website/template/head.php
@@ -33,7 +33,7 @@
                     </div>
                 </div>
             </header>
-            <div class="nav-scroller py-1 mb-2">
+            <div class="nav-scroller" style="border-bottom: 1px solid rgba(0,0,0,.1);">
                 <nav class="nav d-flex"> <?php /* justify-content-between */ ?>
                     <?php
                         $category_list = CategoryM::new()->getList();
@@ -43,12 +43,6 @@
                     <?php } ?>
                 </nav>
             </div>
-            <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-                <div class="col-md-6 px-0">
-                    <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
-                    <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
-                    <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
-                </div>
-            </div>
+            <br />
         </div>
         <main role="main" class="container">
